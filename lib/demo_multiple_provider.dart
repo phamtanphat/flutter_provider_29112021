8e97d547705874cc6f77a1bf8b570a0a1104bb48
phamtanphat <phatdroid94@gmail.com>
2022-01-12 20:56:01 +0700

    multiprovider

--- lib/demo_multiple_provider.dart
+++ lib/demo_multiple_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Counter1 extends ChangeNotifier{
   late int value;
@@ -8,6 +9,7 @@ class Counter1 extends ChangeNotifier{
 
   void inCrease(){
     value += 1;
+    notifyListeners();
   }
 }
 
@@ -16,8 +18,9 @@ class Counter2 extends ChangeNotifier{
 
   Counter2({required this.value});
 
-  void inCrease(){
+  void deCrease(){
     value -= 1;
+    notifyListeners();
   }
 }
 
@@ -31,12 +34,22 @@ class DemoMultipleProvider extends StatelessWidget {
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Child1Widget(),
-            Child2Widget()
+        child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (context) => Counter1(value: 0),
+            ),
+            ChangeNotifierProvider(
+              create: (context) => Counter2(value: 0),
+            )
           ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Child1Widget(),
+              Child2Widget()
+            ],
+          ),
         ),
       ),
     );
@@ -48,11 +61,17 @@ class Child1Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text("Counter 1 = 0", style: TextStyle(fontSize: 20)),
-          ElevatedButton(onPressed: (){}, child: Text("InCrease"))
-        ],
+      child: Consumer<Counter1>(
+        builder: (context , counter1 , child){
+          return Column(
+            children: [
+              Text("Counter 1 = ${counter1.value}", style: TextStyle(fontSize: 20)),
+              ElevatedButton(onPressed: (){
+                counter1.inCrease();
+              }, child: Text("InCrease"))
+            ],
+          );
+        },
       ),
     );
   }
@@ -63,11 +82,17 @@ class Child2Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text("Counter 2 = 0" , style: TextStyle(fontSize: 20)),
-          ElevatedButton(onPressed: (){}, child: Text("DeCrease"))
-        ],
+      child: Consumer<Counter2>(
+        builder: (context , counter2 , child){
+          return Column(
+            children: [
+              Text("Counter 2 = ${counter2.value}", style: TextStyle(fontSize: 20)),
+              ElevatedButton(onPressed: (){
+                counter2.deCrease();
+              }, child: Text("InCrease"))
+            ],
+          );
+        },
       ),
     );
   }
