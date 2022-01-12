a172558dc09108f40aec898a57e5afb4c8d5c7e9
phamtanphat <phatdroid94@gmail.com>
2022-01-12 20:32:10 +0700

    get counter

--- lib/demo_changenotifier_provider.dart
+++ lib/demo_changenotifier_provider.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Counter extends ChangeNotifier {
+  late int value;
+
+  Counter({required this.value});
+
+  void inCreaseCount() {
+    value += 1;
+  }
+}
 
 class DemoChangeNotifier extends StatefulWidget {
-
   @override
   _DemoChangeNotifierState createState() => _DemoChangeNotifierState();
 }
@@ -15,13 +25,13 @@ class _DemoChangeNotifierState extends State<DemoChangeNotifier> {
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextWidget(),
-            ButtonWidget()
-          ],
+        child: ChangeNotifierProvider<Counter>(
+          create: (context) => Counter(value: 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [TextWidget(), ButtonWidget()],
+          ),
         ),
       ),
     );
@@ -29,25 +39,19 @@ class _DemoChangeNotifierState extends State<DemoChangeNotifier> {
 }
 
 class TextWidget extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Text("");
-  }
-}
-
-
-class ButtonWidget extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: (){
-
-        },
-        child: Text("Increase")
+    return Consumer<Counter>(
+        builder: (context , counter , child){
+          return Text(counter.value.toString());
+        }
     );
   }
 }
 
-
+class ButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text("Increase"));
+  }
+}
