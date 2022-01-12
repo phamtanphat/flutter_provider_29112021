e05f64f778a35a4a968dfe02c96a52b1c2f24184
phamtanphat <phatdroid94@gmail.com>
2022-01-12 21:00:47 +0700

    ui listenable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_provider_29112021/demo_basic_provider.dart';
 import 'package:flutter_provider_29112021/demo_changenotifier_provider.dart';
+import 'package:flutter_provider_29112021/demo_listenable_provider.dart';
 import 'package:flutter_provider_29112021/demo_multiple_provider.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DemoMultipleProvider(),
+      home: DemoValueListenable(),
     );
   }
 }
