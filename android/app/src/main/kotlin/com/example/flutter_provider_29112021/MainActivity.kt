00757acf997255779b96903b6b467651fa47bc26
phamtanphat <phatdroid94@gmail.com>
2022-01-12 19:58:35 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_provider_29112021/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_provider_29112021
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/demo_basic_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class DemoBasicProvider extends StatefulWidget {
+
+  @override
+  _DemoBasicProviderState createState() => _DemoBasicProviderState();
+}
+
+class _DemoBasicProviderState extends State<DemoBasicProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Basic Provider"),
+      ),
+      body: Container(),
+    );
+  }
+}
