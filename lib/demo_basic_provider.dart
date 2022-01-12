5ef716e7418ec9108b41d9a65d8053271498e1a5
phamtanphat <phatdroid94@gmail.com>
2022-01-12 20:15:39 +0700

    basic

--- lib/demo_basic_provider.dart
+++ lib/demo_basic_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 class DemoBasicProvider extends StatefulWidget {
 
   @override
@@ -6,13 +7,55 @@ class DemoBasicProvider extends StatefulWidget {
 }
 
 class _DemoBasicProviderState extends State<DemoBasicProvider> {
+
+  String text = "Phat";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Demo Basic Provider"),
       ),
-      body: Container(),
+      body: Container(
+        child: Provider.value(
+          value: text,
+          child: Column(
+            children: [
+              Child1Widget(),
+              Child2Widget(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+
+class Child1Widget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    String text = Provider.of(context);
+    return Container(
+      child: Text(text),
+    );
+  }
+}
+
+class Child2Widget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Consumer<String>(
+          builder: (context , text , child){
+            return Text(text);
+          }
+      ),
+    );
+  }
+}
+
+
+
