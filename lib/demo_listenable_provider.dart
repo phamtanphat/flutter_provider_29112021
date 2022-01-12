8f3c3767c5c87b7a6ee551c33532536f15d624fb
phamtanphat <phatdroid94@gmail.com>
2022-01-12 21:14:24 +0700

    demo valuelistenable

--- lib/demo_listenable_provider.dart
+++ lib/demo_listenable_provider.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Counter{
+  ValueNotifier<int> count = ValueNotifier(0);
+}
 
 class DemoValueListenable extends StatelessWidget {
 
@@ -8,7 +14,42 @@ class DemoValueListenable extends StatelessWidget {
       appBar: AppBar(
         title: Text("Demo Valuelistenable Provider"),
       ),
-      body: Container(),
+      body: Provider(
+        create: (context) => Counter(),
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          child: Consumer<Counter>(
+            builder: (context , counter , child){
+              return ValueListenableProvider.value(
+                value: counter.count,
+                child: Consumer<int>(
+                  builder: (context , count , child){
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text("Count : $count"),
+                        ElevatedButton(
+                            onPressed: (){
+                              counter.count.value += 1;
+                            },
+                            child: Text("+")
+                        ),
+                        ElevatedButton(
+                            onPressed: (){
+                              counter.count.value -= 1;
+                            },
+                            child: Text("-")
+                        )
+                      ],
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
