bf398987709a0ecf3caaa5644f822598452d85e3
phamtanphat <phatdroid94@gmail.com>
2022-01-14 21:18:18 +0700

    proxy

--- lib/demo_proxy_provider.dart
+++ lib/demo_proxy_provider.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-class Counter{
+class Counter extends ChangeNotifier{
   late int value;
+  late String text;
 
   Counter({required this.value});
+
+
+  void increase(){
+    value += 1;
+    notifyListeners();
+  }
+
+  void decrease(){
+    value -= 1;
+    notifyListeners();
+  }
 
 }
 
@@ -35,28 +47,42 @@ class DemoProxyProvider extends StatelessWidget {
       ),
       body: MultiProvider(
         providers: [
-          Provider(create: (context) => Counter(value: 10)),
+          ChangeNotifierProvider(create: (context) => Counter(value: 0)),
           ProxyProvider<Counter,Request>(
             create: (context) => Request(),
-            update: (context,counter,request){
-              request!.updateCounter(counter);
-              return request;
+            update: (context,counter,requestPrevious){
+              requestPrevious = Request()..updateCounter(counter);
+              return requestPrevious;
             },
           ),
           ProxyProvider<Request,Repository>(
               create: (context) => Repository(),
               update: (context , request , repository){
-                repository!.updateRequest(request);
+                repository = Repository()..updateRequest(request);
                 return repository;
-              }
+              },
           )
         ],
         child: Consumer<Repository>(
           builder: (context , repository , child){
             return Container(
               constraints: BoxConstraints.expand(),
-              child: Center(
-                child: Text(repository.request.counter.value.toString()),
+              child: Column(
+                children: [
+                  Text(repository.request.counter.value.toString()),
+                  ElevatedButton(
+                      onPressed: (){
+                        repository.request.counter.increase();
+                      },
+                      child: Text("+")
+                  ),
+                  ElevatedButton(
+                      onPressed: (){
+                        repository.request.counter.decrease();
+                      },
+                      child: Text("-")
+                  )
+                ],
               ),
             );
           },
